818bf8a25a36498d385d457499bda8ad91a08662
Josh Bain <joshuambain@gmail.com>
2024-08-29 14:07:12 -0400

    complete conditional statements lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    return "Access granted" if username.lower() == "admin" and password == "12345" else "Access denied"
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
+def hows_the_weather(temperature): 
+    if temperature <= 40:
+        description = "brisk"
+    elif 40 < temperature < 65: 
+        description = "a little chilly"
+    elif temperature > 85:
+        description = "too dang hot"
+    else:
+        description = "perfect"
+    return f"It's {description} out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0 and num % 5 > 0:
+        return "Fizz"
+    elif num % 3 > 0 and num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+# operations = {
+#     "+": +,
+#     "-": -,
+#     "/": /,
+#     "*": *
+# }
 
 def calculator(operation, num1, num2):
-    # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
     pass
